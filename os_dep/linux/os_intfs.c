1f0867dd5b6eed2349157b16573ae5c540b3caa1
zexceed12300 <36181923+zexceed12300@users.noreply.github.com>
2019-12-23 20:21:51 -0500

    fix some compile error

--- os_dep/linux/os_intfs.c
+++ os_dep/linux/os_intfs.c
@@ -1295,10 +1295,11 @@ static u16 rtw_select_queue(struct net_device *dev, struct sk_buff *skb,
 #elif (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 19, 0))
 static u16 rtw_select_queue(struct net_device *dev, struct sk_buff *skb,
 			    struct net_device *sb_dev,
-			    select_queue_fallback_t fallback)
+			    struct select_queue_fallback_t *fallback)
 #else
 static u16 rtw_select_queue(struct net_device *dev, struct sk_buff *skb,
-			    void *accel_priv, select_queue_fallback_t fallback)
+			    void *accel_priv, 
+			    struct select_queue_fallback_t *fallback)
 #endif
 {
 	_adapter	*padapter = rtw_netdev_priv(dev);
